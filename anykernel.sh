e68f562eb89943eef2a6daf5b9630dfb030ff020
rajauser305 <rajabulloh1@gmail.com>
2026-02-12 21:15:01 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Kernels by TheWildJames aka Morgan Weedman
+kernel.string=FUCK YOU BITCH
 do.devicecheck=0
 do.modules=0
 do.systemless=0
